8ae090263d1fcb3142d561a0706c46940db28e39
Marcos Vinicius Alves <marcolinoloko1411@gmail.com>
2026-09-15 16:56:05 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+horas = float(input("Quantas horas ela trabalhou? "))
+valor_hora = float(input("Quanto ela recebe por hora? R$ "))
+
+salario_total = horas * valor_hora
+
+print(f"O salário total será: R$ {salario_total:.2f}")
